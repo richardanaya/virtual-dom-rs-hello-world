95fe3a45ae3d312f525f7cb593d2362fd1025e24
Richard <Richard>
2018-12-29 21:59:13 +0000

    first push

---
+++ README.md
@@ -0,0 +1,20 @@
+# Hello World with virtual-dom-rs
+
+This was one of the coolest libraries i've seen doing virtual DOM in Rust. I was disappointed though it didn't have any simple examples nor is the crate on cargo up to date. So I made this project to show the most simplest utilization of a client side app using entirely wasm-bindgen, web-sys, and virtual-dom-rs.
+
+No javascript required (asside what wasm-bindgen auto creates ;) )!
+
+You are going to need some things unfortunately:
+* cargo with nightly and wasm target
+* nodejs + npm ( this is a dependency of wasm-bindgen)
+* make
+
+How to run:
+
+```console
+make setup
+make
+make serve
+```
+
+if you want to edit it after its running, just open up a new tab and run `make` to generate new wasm binary, you shouldn't have to restart the server.
---
+++ src/lib.rs
@@ -0,0 +1,74 @@
+#[macro_use]
+extern crate virtual_dom_rs;
+use wasm_bindgen::prelude::*;
+use virtual_dom_rs::VirtualNode;
+use virtual_dom_rs::JsCast;
+
+// HelloWorld component just sys hello along with a global counter value
+struct HelloWorld {}
+
+static mut COUNT:i32 = 0;
+
+impl HelloWorld {
+    fn new() ->  HelloWorld {
+        HelloWorld{}
+    }
+
+    fn render(&self) -> VirtualNode {
+        // it's unsafe to even read global statics
+        let c = unsafe {
+            COUNT
+        };
+
+        html! {
+            <div>
+                { format!("Hello World!! {}",c)}
+            </div>
+        }
+    }
+}
+
+
+pub fn render_to_dom(root_element:web_sys::Element,previous_vdom:&Option<VirtualNode>,new_vdom:&mut VirtualNode) {
+    if let Some(p_vd) = previous_vdom {
+        // If its not the first time, calculate the DOM dif and apply to root dom contents
+        let patches = virtual_dom_rs::diff(&p_vd, new_vdom);
+        virtual_dom_rs::patch(root_element, &patches);
+    }  else {
+        // If its the first time just set the contents of the DOM to string
+        root_element.set_inner_html(&new_vdom.to_string());
+    }
+}
+
+#[wasm_bindgen(start)]
+pub fn run() -> Result<(), JsValue> {
+    // Let's first get the body since this is going to be our root node
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+
+    let hello_world = HelloWorld::new();
+
+    // Originally we have no DOM and count is 0, let's do our first render
+    let previous_vdom = None;
+    let mut new_vdom = hello_world.render();
+    render_to_dom(virtual_dom_rs::Element::from(body), &previous_vdom,&mut new_vdom);
+
+    // Now we are going to increment the counter and render every second
+    // So, this looks really complicated, but basically we are just
+    // 1. creating a complicated reference of a Closure
+    // 2. saying we want it called ever 1000 ms (1 second)
+    // 3. then forgetting it so that it doesn't get dropped and cause an error the next timer
+    let a = Closure::wrap(Box::new(move || {
+        unsafe {
+            COUNT += 1;
+        }
+        let mut new_vdom = hello_world.render();
+        let body = document.body().expect("document should have a body");
+        render_to_dom(virtual_dom_rs::Element::from(body), &previous_vdom,&mut new_vdom);
+    }) as Box<dyn Fn()>);
+    window.set_interval_with_callback_and_timeout_and_arguments_0(a.as_ref().unchecked_ref(), 1000)?;
+    a.forget();
+
+    Ok(())
+}
